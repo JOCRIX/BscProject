e2f9c2bf6460f1e51215327c0fd6c2e993a9e96a
JRIX <43089857+JOCRIX@users.noreply.github.com>
2024-10-23 11:20:07 +0200

    upd

--- Code/VHDL/ADCControl/ADCControl.srcs/sources_1/new/adc_control.vhd
+++ Code/VHDL/ADCControl/ADCControl.srcs/sources_1/new/adc_control.vhd
@@ -36,11 +36,11 @@ entity adc_control is
             PULSE_CLK_SPI_PULSEGEN_OUT_TO_ADC_CONTROL_IN        : in std_logic  := '0';
             PULSE_BUSY_PULSEGEN_TO_ADC_CONTROL_IN               : in std_logic  := '0';
             PULSE_COMPLETE_PULSEGEN_TO_ADC_CONTROL_IN           : in std_logic  := '0';
-            SDA_POS_ADC_A_TO_ADC_CONTROL_IN                     : in std_logic  := '0';
-            SDA_POS_ADC_B_TO_ADC_CONTROL_IN                     : in std_logic  := '0';   
-            SCK_POS_ADC_CONTROL_TO_ADC_A_OUT                    : out std_logic := '0';
-            SCK_POS_ADC_CONTROL_TO_ADC_B_OUT                    : out std_logic := '0';
-            CNV_ACQUIRE_ADC_CONTROL_TO_ADC_A_AND_B_OUT          : out std_logic := '0';
+            EXT_SDA_POS_ADC_A_TO_ADC_CONTROL_IN                 : in std_logic  := '0';
+            EXT_SDA_POS_ADC_B_TO_ADC_CONTROL_IN                 : in std_logic  := '0';   
+            EXT_SCK_POS_ADC_CONTROL_TO_ADC_A_OUT                : out std_logic := '0';
+            EXT_SCK_POS_ADC_CONTROL_TO_ADC_B_OUT                : out std_logic := '0';
+            EXT_CNV_ACQUIRE_EXT_ADC_CONTROL_TO_ADC_A_AND_B_OUT  : out std_logic := '0';
             ADC_A_DATA_ADC_CONTROL_TO_IV_SAVER_OUT              : out std_logic_vector(15 downto 0) := (others => '0');
             ADC_B_DATA_ADC_CONTROL_TO_IV_SAVER_OUT              : out std_logic_vector(15 downto 0) := (others => '0');
             ADC_A_AND_B_DATA_READY_ADC_CONTROL_TO_IV_SAVER_OUT  : out std_logic := '0';
@@ -52,6 +52,18 @@ architecture Behavioral of adc_control is
 
 
 begin
+
+adc_acquire : process (ADC_ACQUIRE_START_INT_MEM_TO_ADC_CONTROL_IN) is
+begin
+if(rising_edge(ADC_ACQUIRE_START_INT_MEM_TO_ADC_CONTROL_IN))then
+            
+end if;
+end process;
+
+--gen_cnv_pulse : process () is
+--begin
+
+--end process;
 
 
 end Behavioral;
