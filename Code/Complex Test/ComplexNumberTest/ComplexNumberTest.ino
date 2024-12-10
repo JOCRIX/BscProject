65619672f8f1e2c7c16b30e9d601a4260c59b8ab
JOCRIX <j.rixen@outlook.dk>
2024-12-10 18:59:10 +0100

    complex math code

--- Code/Complex Test/ComplexNumberTest/ComplexNumberTest.ino
+++ Code/Complex Test/ComplexNumberTest/ComplexNumberTest.ino
@@ -1,44 +1,53 @@
+#include <math.h>
 
 struct complex {
   double imag;
   double real;
 };
-
 struct complexMath{
-  
-};
+  double  (*argz)(complex);
+  double  (*argzDeg)(complex);
+  double  (*magz)(complex);
+}cmplxmath;
 
 complex z;
 
 void setup() {
   Serial.begin(115200);
-  z.imag = 1.3;
-  z.real = 5.4;
-
-
+  z.imag = -1;
+  z.real = 1;
+  cmplxmath.argz = argz;
+  cmplxmath.argzDeg = argzDeg;
+  cmplxmath.magz = magz;
 }
 
 void loop() {
-  Serial.println(atan(z.imag/z.real));
+  Serial.print("ARGZ : ");
+  Serial.print(cmplxmath.argz(z));
+  Serial.println();
   delay(1000);
-  // put your main code here, to run repeatedly:
+
+  Serial.print("ARGZDEG : ");
+  Serial.print(cmplxmath.argzDeg(z));
+  Serial.println();
+  delay(1000);
+
+    Serial.print("MAGZ : ");
+  Serial.print(cmplxmath.magz(z));
+  Serial.println();
+  delay(1000);
+
 
 }
-´
-double argc(complex )
+double argz(complex z)
 {
-  return atan2()
+  return atan2(z.imag, z.real); //atan2() tager højde for og korrigerer tangens fejl.
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
+double argzDeg(complex z)
+{
+  return atan2(z.imag, z.real) * (180/M_PI); //atan2() tager højde for og korrigerer tangens fejl.
+}
+double magz(complex z)
+{
+  return sqrt(z.real*z.real + z.imag*z.imag);
+}
