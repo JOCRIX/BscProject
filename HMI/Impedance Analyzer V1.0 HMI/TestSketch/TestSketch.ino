28cf0fc4fdc7ce0c6c25884cd1ac385026898f63
JOCRIX <j.rixen@outlook.dk>
2024-12-21 02:24:39 +0100

    lol

--- HMI/Impedance Analyzer V1.0 HMI/TestSketch/TestSketch.ino
+++ HMI/Impedance Analyzer V1.0 HMI/TestSketch/TestSketch.ino
@@ -7,13 +7,22 @@ void setup() {
   Serial.begin(115200);
   pinMode(A5, OUTPUT);
   digitalWrite(A5, LOW);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
 //Serial.println("Enter data:");
-      while (Serial.available() == 0) {}     //wait for data available
+      while (Serial.available() == 0) {
+        digitalWrite(LED_BUILTIN, LOW);
+        
+        }     //wait for data available
         String teststr = Serial.readString();  //read until timeout
         teststr.trim();                        // remove any \r \n whitespace at the end of the String
+
+     Serial.println(teststr);
+     digitalWrite(LED_BUILTIN, HIGH);
+     delay(1000);
+    /*  
       if (teststr == "RunBtn") {
        // Serial.println("A primary color");
        test = !test;
@@ -21,5 +30,5 @@ void loop() {
        Serial.println("test");
       } else {
         //Serial.println("Something else");
-      }
+      }*/
 }
